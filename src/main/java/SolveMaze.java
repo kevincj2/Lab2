d09ca46f716cf98a53aeff2fa00b677cdaeb151c
Kevin Jones <kevincj2@illinois.edu>
2018-01-30 12:24:24 -0600

    maze solver

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,13 +34,26 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                if (maze.canMove() == false) {
+                    maze.turnLeft();
+                }
+                if (maze.canMove() == false) {
+                    maze.turnLeft();
+                    maze.turnLeft();
+                } else {
+                    maze.move();
+                }
+            }
+                maze.move();
+            }
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
+            }
         }
 
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
-        }
+
     }
-}
+
